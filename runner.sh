33fef900f185cd58aa574b7efb190ea23eb6ffed
Yuchi(Allan) Zhao <43657501+y556zhao@users.noreply.github.com>
2022-06-13 00:29:19 -0400

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 export PYTHONPATH=$(readlink -f "${SCRIPT_DIR}")
 export OPENBLAS_NUM_THREADS=1
 
-$SCRIPT_DIR/env/bin/python $@
+h/helen/envs/transparent-perception/bin/python $@
